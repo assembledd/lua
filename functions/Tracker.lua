7b37991bb7f5db71b18cbd7d7f7dc65b1a659f15
assembled <assembled48@gmail.com>
2026-02-23 06:17:26 +0100

    Update and rename tracker to Tracker.lua

--- functions/Tracker.lua
+++ functions/Tracker.lua
@@ -1,6 +1,6 @@
-local env = getgenv and getgenv() or _G
-if env.tracker then
+local Env = getgenv and getgenv() or _G
+if Env.tracker then
     pcall(function() game:GetService("Players").LocalPlayer:Kick("Already Injected.\nPlease Rejoin!") end)
     task.wait(9e9)
 end
-env.tracker = 1
+Env.tracker = 1
