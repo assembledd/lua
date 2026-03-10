51772c147367612ef85d988a0e13b5112f6bba46
assembled <assembled48@gmail.com>
2026-03-10 05:02:23 +0100

    Delete functions directory

--- functions/Tracker.lua
+++
@@ -1,6 +0,0 @@
-local Env = getgenv and getgenv() or _G
-if Env.Tracker then
-    pcall(function() game:GetService("Players").LocalPlayer:Kick("Already Injected.\nPlease Rejoin!") end)
-    task.wait(9e9)
-end
-Env.Tracker = 1
